8fc420da9e0b9b4e469dc6bbf7e0e7e9d7ac080c
z03h <earkz0@gmail.com>
2021-08-29 07:48:16 -0700

    update read_messages to an alias for view_channel

--- discord/permissions.py
+++ discord/permissions.py
@@ -332,15 +332,22 @@ class Permissions(BaseFlags):
         return 1 << 9
 
     @flag_value
-    def read_messages(self) -> int:
-        """:class:`bool`: Returns ``True`` if a user can read messages from all or specific text channels."""
+    def view_channel(self) -> int:
+        """:class:`bool`: Returns ``True`` if a user can read messages from all or specific text channels.
+
+        .. versionadded:: 1.3
+
+        .. versionchanged:: 2.0
+            No longer an alias for :attr:`read_messages`.
+        """
         return 1 << 10
 
     @make_permission_alias('read_messages')
-    def view_channel(self) -> int:
-        """:class:`bool`: An alias for :attr:`read_messages`.
+    def read_messages(self) -> int:
+        """:class:`bool`: An alias for :attr:`view_channel`.
 
-        .. versionadded:: 1.3
+        .. versionchanged:: 2.0
+            Is now an alias for :attr:`view_channel`.
         """
         return 1 << 10
 
