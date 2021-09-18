87b2b5e069311785f4abd75685c71f22d61f616c
jay3332 <40323796+jay3332@users.noreply.github.com>
2021-09-18 10:44:41 -0400

    Discourage nested classes in ``slash_commands`` example
    Signed-off-by: jay3332 <40323796+jay3332@users.noreply.github.com>

--- examples/slash_commands.py
+++ examples/slash_commands.py
@@ -5,7 +5,7 @@ tree = ApplicationCommandTree(guild_id=1234)  # Replace with your guild ID, or `
 
 
 class Ping(ApplicationCommand, name='ping', tree=tree):
-    """Pong?"""
+    """Pong!"""
 
     async def callback(self, interaction: discord.Interaction):
         await interaction.response.send_message('Pong!')
@@ -14,29 +14,25 @@ class Ping(ApplicationCommand, name='ping', tree=tree):
 class Math(ApplicationCommand, name='math', tree=tree):
     """Basic math operations."""
 
-    class Add(ApplicationCommand, name='add'):
-        """Sum of x + y."""
-        x: int = option(description='Value of "x"', required=True)
-        y: int = option(description='Value of "y"', required=True)
 
-        async def callback(self, interaction: discord.Interaction):
-            answer = self.x + self.y
-            await interaction.response.send_message(
-                f'The value of {self.x} + {self.y} is **{answer}**.',
-                ephemeral=True
-            )
+class Add(ApplicationCommand, name='add', parent=Math):
+    """Sum of x + y."""
+    x: int = option(description='Value of "x"', required=True)
+    y: int = option(description='Value of "y"', required=True)
 
-    class Subtract(ApplicationCommand, name='subtract'):
-        """Difference of x - y."""
-        x: int = option(description='Value of "x"', required=True)
-        y: int = option(description='Value of "y"', required=True)
+    async def callback(self, interaction: discord.Interaction):
+        answer = self.x + self.y
+        await interaction.response.send_message(f'The value of {self.x} + {self.y} is **{answer}**.', ephemeral=True)
 
-        async def callback(self, interaction: discord.Interaction):
-            answer = self.x - self.y
-            await interaction.response.send_message(
-                f'The value of {self.x} - {self.y} is **{answer}**.',
-                ephemeral=True
-            )
+
+class Subtract(ApplicationCommand, name='subtract', parent=Math):
+    """Difference of x - y."""
+    x: int = option(description='Value of "x"', required=True)
+    y: int = option(description='Value of "y"', required=True)
+
+    async def callback(self, interaction: discord.Interaction):
+        answer = self.x - self.y
+        await interaction.response.send_message(f'The value of {self.x} - {self.y} is **{answer}**.', ephemeral=True)
 
 
 class Client(discord.Client):
